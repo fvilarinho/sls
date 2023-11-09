a305d4c6f5b2bf772d8f7b70abac8f280bc181d3
fvilarin <fvilarin@akamai.com>
2023-11-09 14:52:10 -0300

    Updates.

---
+++ iac/bin/startup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+function checkDependencies() {
+  SLS_CMD=$(which sls)
+
+  if [ ! -f "$SLS_CMD" ]; then
+    echo "sls binary was not found! Please check your installation!"
+
+    exit 1
+  fi
+}
+
+function prepareToExecute() {
+  cp -f "$ETC_DIR"/sls.conf.template "$ETC_DIR"/sls.conf
+
+  sed -i -e 's|${THREADS}|'"$THREADS"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${CONNECTIONS}|'"$CONNECTIONS"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${LISTEN_PORT}|'"$LISTEN_PORT"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${LATENCY}|'"$LATENCY"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${BACKLOG}|'"$BACKLOG"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${IDLE_TIMEOUT}|'"$IDLE_TIMEOUT"'|g' "$ETC_DIR"/sls.conf
+  sed -i -e 's|${LOGS_DIR}|'"$LOGS_DIR"'|g' "$ETC_DIR"/sls.conf
+}
+
+function start() {
+  $SLS_CMD -c "$ETC_DIR"/sls.conf
+}
+
+function main() {
+  checkDependencies
+  prepareToExecute
+  start
+}
+
+main
